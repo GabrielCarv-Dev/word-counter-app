62e342a00cd79c7a46df736c277c820745f42695
GabrielCarv-Dev <224664621+GabrielCarv-Dev@users.noreply.github.com>
2025-08-14 17:24:30 -0300

    feat(api): adiciona rota /ping e corrige dependecias no requirement

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
-
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"message": "Hello, DevOps!"}
+
+@app.get("/ping")
+def ping():
+    return JSONResponse(content={"ping": "pong"})
